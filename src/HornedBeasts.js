f011d2ababce14a4b58b804186453fbaa93826db
Marie <marie.marcos25@gmail.com>
2021-03-25 11:47:28 -0700

    added bootstrap

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,30 +1,40 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 
 class HornedBeasts extends React.Component {
 
-  constructor (props){
+  constructor(props) {
     super(props);
 
     this.state = {
       numberOfClicks: 0,
       numberOfHearts: '',
-    }
+    };
   }
 
   vote = () => {
-    this.setState({numberofClicks: this.state.numberofClicks + 1});
-    this.setState({numberOfHearts:''});
+    this.setState({ numberofClicks: this.state.numberofClicks + 1 });
+    this.setState({ numberOfHearts: '💚' });
   }
 
-  render (){
+  render() {
     return (
-      <div>
-        <img src={this.props.imgUrl} alt={this.props.alt} title={this.props.title}></img>
-        <h2>{this.props.title}</h2>    
+      <div key={this.props.keyword}>
+        <h2>{this.props.title}</h2>
+        <p>
+          {this.state.numberOfClicks}
+          {this.state.numberOfHearts}
+        </p>
+
+        <img src={this.props.img_url}
+          description={this.props.description}
+          title={this.props.title}
+          alt={this.props.title}
+        />
+
         <p>{this.props.description}</p>
-        {/* <Button onClick = { () => this.setState({
-          numberofClicks: this.state.numberOfClicks + ''} >Vote </Button> */}
+        <Button onClick={this.vote}>Vote Here</Button>
 
       </div>
     );
